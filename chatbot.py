b3b88551122c1ddccb25a0ff530f0299df18edbb
Andreas Loutzidis <aloutzidis@gmail.com>
2023-11-19 16:04:31 -0500

    added chatbot implementation

---
+++ chatbot.py
@@ -0,0 +1,54 @@
+import logging
+
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import (ChatPromptTemplate, HumanMessagePromptTemplate,
+                               MessagesPlaceholder,
+                               SystemMessagePromptTemplate)
+
+logging.basicConfig(level=logging.INFO)
+# LLM
+llm = ChatOpenAI(max_tokens=150, temperature=0.9)
+
+# Prompt
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a door-to-door salesperson expert. You are teaching a new recruit how to sell."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ]
+)
+
+
+# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+# Notice that `"chat_history"` aligns with the MessagesPlaceholder name
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(llm=llm, prompt=prompt, verbose=False, memory=memory)
+
+print("Welcome to the door-to-door salesperson expert chatbot!")
+print("Type 'history' to see the conversation history.")
+print("Press ctrl-c or ctrl-d on the keyboard to exit.")
+print("")
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        user_input = input("> ")
+        if user_input == "history":
+            print(conversation.memory.chat_memory)
+            continue
+
+        print("Processing...")
+
+        # Simulate model response time
+        # time.sleep(2)
+        bot_response = conversation({"question": user_input})
+        print(bot_response["text"])
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
