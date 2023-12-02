bfca8f749cc6e78081114a4dae611fa48feef2b9
Andreas Loutzidis <aloutzidis@gmail.com>
2023-12-02 16:39:32 -0500

    implements tracker of personas

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -6,6 +6,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.prompts import (ChatPromptTemplate, HumanMessagePromptTemplate,
                                MessagesPlaceholder,
                                SystemMessagePromptTemplate)
+from personas import is_persona_prompt
 
 logging.basicConfig(level=logging.ERROR)
 # LLM
@@ -30,6 +31,28 @@ prompt = ChatPromptTemplate(
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 conversation = LLMChain(llm=llm, prompt=prompt, verbose=False, memory=memory)
 
+
+personas_memory = ConversationBufferMemory(memory_key="personas_chat_history", return_messages=True)
+personas_system_message = f"""Your job is to determine if the user asks you to take a persona or role in your response. It might ask you that you are an animal, person or something else. If it indeed asks you for you to take a persona, return the role that its ask you to take only. Otherwise return "NONE". for example if the user says: pretend to be a pirate then you should return pirate"""
+personas_prompt = ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(personas_system_message),
+            MessagesPlaceholder(variable_name="personas_chat_history"),
+            HumanMessagePromptTemplate.from_template("{question}"),
+        ]
+    )
+llm = ChatOpenAI(max_tokens=200, temperature=0.9)
+conversation_personas = LLMChain(llm=llm, prompt=personas_prompt, verbose=False, memory=personas_memory)
+personas_list = []
+def is_persona_prompt(user_prompt: str):
+    """ Takes user's input and prints whether the input asks the model 
+    to take on a specific persona (pirate, greek god)"""
+    response = conversation_personas({"question": user_prompt})["text"]
+    if response == 'NONE':
+        return
+    else:
+        personas_list.append(response)
+        
 print(f"Welcome to the door-to-door {field_of_work} salesperson expert chatbot!")
 print("Type 'history' to see the conversation history.")
 print("Press ctrl-c or ctrl-d on the keyboard to exit.")
@@ -39,11 +62,15 @@ print("")
 while True:
     try:
         user_input = input("> ")
+        is_persona_prompt(user_input)
         if user_input == "history":
             for m in conversation.memory.chat_memory.messages:
                 print(m.content)
             continue
-
+        elif user_input == "personas":
+            for p in personas_list:
+                print(p)
+            continue
         print("Processing...")
 
         bot_response = conversation({"question": user_input})
